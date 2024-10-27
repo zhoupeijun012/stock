71887b8cb2e88b5b55193431e775c9610afe29f6
zhoupeijun012 <zhoupeijun012@163.com>
2024-10-27 19:43:14 +0800

    2323

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # 使用官方的Node.js基础镜像
-FROM node:18
+FROM node:18-alpine3.18
 
 # 设置工作目录
 WORKDIR /usr/src/app
--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ module.exports = {
   GET_URL: "http://aktools.amdyes.asia",
   DB_USER: 'root',
   DB_PWD: 'Zz7811181',
-  DB_URL: 'localhost',
+  DB_URL: '152.70.148.68',
   DB_NAME: 'stock',
   TABLE_PREFIX:'T_',
   SERVER_PORT: 12345
