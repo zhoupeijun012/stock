46f0f3f37c12bfffb5da31ac23c4abd919756ea8
zhoupeijun012 <zhoupeijun012@163.com>
2025-03-19 17:08:22 +0800

    优化

--- spider/model/concept.js
+++ spider/model/concept.js
@@ -66,6 +66,47 @@ class Concept extends require("./base-query") {
       pages,
     };
   }
+  queryPage(params) {
+    const { pageNum, pageSize, matchKey = [], order = [], where = {} } = params;
+    const tableOrders = order.map((item) => {
+      if (item.prop == "10086") {
+      } else {
+        return [
+          cast(col(item.prop), "SIGNED"),
+          item.order == "ascending" ? "ASC" : "DESC",
+        ];
+      }
+    });
+
+    const whereArr = [];
+    for (let key of Object.keys(where)) {
+      // 股票名称
+      if (key == "c1") {
+        whereArr.push({
+          [key]: {
+            [Op.eq]: where[key],
+          },
+        });
+      } else {
+        whereArr.push({
+          [key]: {
+            [Op.like]: `%${where[key]}%`,
+          },
+        });
+      }
+    }
+
+    const whereMap = {
+      [Op.and]: whereArr,
+    };
+    return super.queryPage({
+      pageNum,
+      pageSize,
+      matchKey,
+      order: tableOrders,
+      where: whereMap,
+    });
+  }
 }
 
 module.exports = new Concept({
