c112b650363223954b51dc89e5de828b2347387f
zhoupeijun012 <zhoupeijun012@163.com>
2025-03-30 16:23:18 +0800

    优化

--- spider/task-queue.js
+++ spider/task-queue.js
@@ -22,89 +22,96 @@ class TaskQueue {
   }
 
   async _execTask() {
-    if (excuting) {
-      return;
-    }
-    excuting = true;
-
-    const data = await taskInstance.queryPage({
-      pageNum: 1,
-      pageSize: this.asyncCount,
-      matchKey: [
-        "taskName",
-        "modelName",
-        "modelFunc",
-        "taskParams",
-        "taskLevel",
-        "retryCount",
-        "message",
-        "needRetry",
-      ],
-      order: [{ order: "ascending", prop: "taskLevel" }],
-      where: {
-        retryCount: 3,
-      },
-    });
-
-    const { list, total } = data;
-    const promiseArr = [];
-    const promiseResult = [];
-    for (let index = 0; index < list.length; index++) {
-      const taskItem = list[index];
-      const modelItem = this.modelList[taskItem.modelName];
-      if (modelItem) {
-        const taskParams = JSON.parse(taskItem["taskParams"]);
-        const promise = modelItem[taskItem["modelFunc"]](taskParams);
-        promiseArr.push(promise);
-        promise
-          .then(() => {
-            promiseResult.push({
-              success: true,
-              uuid: taskItem.uuid,
-            });
-          })
-          .catch((error) => {
-            promiseResult.push({
-              success: false,
-              uuid: taskItem.uuid,
-              needRetry: taskItem.needRetry,
-              retryCount: parseInt(taskItem.retryCount) + 1 + "",
-              message: error.message,
-            });
-          });
-      }
-    }
-
     try {
-      await Promise.allSettled(promiseArr);
+      if (excuting) {
+        return;
+      }
+      excuting = true;
+
+      const data = await taskInstance.queryPage({
+        pageNum: 1,
+        pageSize: this.asyncCount,
+        matchKey: [
+          "taskName",
+          "modelName",
+          "modelFunc",
+          "taskParams",
+          "taskLevel",
+          "retryCount",
+          "message",
+          "needRetry",
+        ],
+        order: [{ order: "ascending", prop: "taskLevel" }],
+        where: {
+          retryCount: 3,
+        },
+      });
+
+      const { list, total } = data;
+      const promiseArr = [];
+      const promiseResult = [];
+      for (let index = 0; index < list.length; index++) {
+        const taskItem = list[index];
+        const modelItem = this.modelList[taskItem.modelName];
+        if (modelItem) {
+          const taskParams = JSON.parse(taskItem["taskParams"]);
+          const promise = modelItem[taskItem["modelFunc"]](taskParams);
+          promiseArr.push(promise);
+          promise
+            .then(() => {
+              promiseResult.push({
+                success: true,
+                uuid: taskItem.uuid,
+              });
+            })
+            .catch((error) => {
+              promiseResult.push({
+                success: false,
+                uuid: taskItem.uuid,
+                needRetry: taskItem.needRetry,
+                retryCount: parseInt(taskItem.retryCount) + 1 + "",
+                message: error.message,
+              });
+            });
+        }
+      }
+
+      try {
+        await Promise.allSettled(promiseArr);
+      } catch (error) {
+        console.log(error.message);
+      }
+
+      // 在这里删除所有成功的的数据
+      const deleteList = promiseResult.filter(
+        (item) => item.success || item.needRetry == "0"
+      );
+      const failList = promiseResult.filter(
+        (item) => !item.success && item.needRetry == "1"
+      );
+      failList.forEach((item) => {
+        delete item.success;
+      });
+
+      await taskInstance.delete({
+        uuid: deleteList.map((item) => item.uuid),
+      });
+
+      // 在这里给所有的错误的数据 + 1
+      await taskInstance.update("uuid", failList);
+
+      if (total > 0) {
+        await TIME_WAIT(200);
+        excuting = false;
+        this._execTask();
+      } else {
+        excuting = false;
+      }
     } catch (error) {
       console.log(error.message);
-    }
-
-    // 在这里删除所有成功的的数据
-    const deleteList = promiseResult.filter(
-      (item) => item.success || item.needRetry == "0"
-    );
-    const failList = promiseResult.filter(
-      (item) => !item.success && item.needRetry == "1"
-    );
-    failList.forEach((item) => {
-      delete item.success;
-    });
-
-    await taskInstance.delete({
-      uuid: deleteList.map((item) => item.uuid),
-    });
-
-    // 在这里给所有的错误的数据 + 1
-    await taskInstance.update("uuid", failList);
-
-    if (total > 0) {
-      await TIME_WAIT(200);
-      excuting = false;
-      this._execTask();
-    } else {
-      excuting = false;
+      WECHAT_SENG_TEXT(error.message).catch((error) => {
+        console.log(error.message);
+      });
     }
   }
   async taskRetry(uuid) {
