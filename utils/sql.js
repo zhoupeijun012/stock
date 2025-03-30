3f4e72cb506c41c158bf34220cd4a4812ad7e637
zhoupeijun012 <zhoupeijun012@163.com>
2025-03-30 14:51:03 +0800

    优化

--- utils/sql.js
+++ utils/sql.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 // 方法 2: 分别传递参数 (sqlite)
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: RESOLVE_PATH("sqlite.db"),
+  storage: RESOLVE_PATH("db/sqlite.db"),
 });
 
 exports.sequelize = sequelize;
