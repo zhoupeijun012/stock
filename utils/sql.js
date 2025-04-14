68617a4c746abff14589b7a1327f806efa304b22
zhoupeijun012 <zhoupeijun012@163.com>
2025-04-14 16:31:25 +0800

    优化

--- utils/sql.js
+++ utils/sql.js
@@ -1,20 +1,7 @@
 const { Sequelize } = require("sequelize");
-// const sequelize = new Sequelize({
-//   dialect: "sqlite",
-//   storage: RESOLVE_PATH("db/sqlite.db"),
-//   logging: false,
-//   pool: {
-//     max: 10, // 连接池最大连接数
-//     min: 5, // 连接池最小连接数
-//     acquire: 30000, // 尝试获取连接的最大时间（毫秒）
-//     idle: 10000 // 连接在连接池中空闲的最大时间（毫秒）
-//   }
-// });
-
-const sequelize = new Sequelize('STOCK','root','Zz7811181', {
-  host: 'mysql.chives.asia',
-  dialect: 'mysql',
-  port: 3306, // MySQL 默认端口
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: RESOLVE_PATH("db/sqlite.db"),
   logging: false,
   pool: {
     max: 10, // 连接池最大连接数
@@ -23,5 +10,18 @@ const sequelize = new Sequelize('STOCK','root','Zz7811181', {
     idle: 10000 // 连接在连接池中空闲的最大时间（毫秒）
   }
 });
+
+// const sequelize = new Sequelize('STOCK','root','Zz7811181', {
+//   host: 'mysql.chives.asia',
+//   dialect: 'mysql',
+//   port: 3306, // MySQL 默认端口
+//   logging: false,
+//   pool: {
+//     max: 10, // 连接池最大连接数
+//     min: 5, // 连接池最小连接数
+//     acquire: 30000, // 尝试获取连接的最大时间（毫秒）
+//     idle: 10000 // 连接在连接池中空闲的最大时间（毫秒）
+//   }
+// });
 
 exports.sequelize = sequelize;
