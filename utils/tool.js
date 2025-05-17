cff364798ddf70585e201348ed1c3dc1985fc97b
zhoupeijun012 <zhoupeijun012@163.com>
2025-05-17 23:36:34 +0800

    优化

--- utils/tool.js
+++ utils/tool.js
@@ -209,7 +209,6 @@ global.stockKMap = (lines = []) => {
       // 成交额
       turnover: parseFloat(splitArr[6]),
       // 涨跌额
-      change: formatMoney(splitArr[8]),
       capital: parseFloat(splitArr[11]),
     };
   };
