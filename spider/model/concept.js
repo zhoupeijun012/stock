c5131f62c3ed3a012f92ada0d6deb1f9eb411993
zhoupeijun012 <zhoupeijun012@163.com>
2025-05-24 16:11:10 +0800

    优化

--- spider/model/concept.js
+++ spider/model/concept.js
@@ -183,7 +183,7 @@ class Concept extends require("./base-query") {
     const pages = Math.ceil(total / diff.length);
     return {
       total,
-      list: diff.filter((item) => item.f2 != "-" && item.f2!="" && item.f3 != "-" && item.f3!=''),
+      list: diff.filter((item) => item.f2 != "-" && item.f2!=="" && item.f3 != "-" && item.f3!==''),
       pageSize: diff.length,
       pageNum: params.pageNum,
       pages,
